4c976e205e05c53029948f360b091f9ad320aa55
kuliaig <kuliaig@users.noreply.github.com>
2026-09-07 06:56:58 +0000

    Deploying to gh-pages from @ kuliaig/OOP@7c85071e507b08803b991b326052d986c458ddac 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Calculator"},{"p":"org.example","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"HeapSort"},{"p":"org.example","l":"Main"}];updateSearchResults();
